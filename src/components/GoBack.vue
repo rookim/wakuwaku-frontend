b46c77b74f975f34cebaab7f4208eba1d88fef57
ro <kronew6@gmail.com>
2022-03-04 18:08:09 -0600

    adds logic for user to go back to previous page

---
+++ src/components/GoBack.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  methods: {
+    goBack: function () {
+      return this.$router.go(-1);
+    },
+  },
+};
+</script>
+
+<template>
+  <span class="go-back">
+    <button @click="goBack()">Back</button>
+  </span>
+</template>
