dc89da65a989655d06a36cb557667f5b909bbec8
ro <kronew6@gmail.com>
2022-03-18 12:50:20 -0500

    adds and sets up vue3-google-oauth2 package

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
+import GAuth from "vue3-google-oauth2";
 
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
@@ -9,6 +10,15 @@ var jwt = localStorage.getItem("jwt");
 if (jwt) {
   axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
 }
+const gAuthOptions = {
+  clientId: "997534777916-v61411mc9t2ukk3p8rc6opmui1veqq2o.apps.googleusercontent.com",
+  scope: "profile email https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events",
+  prompt: "consent",
+  fetch_basic_profile: true,
+};
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.use(GAuth, gAuthOptions);
+app.mount("#app");
 // App.config.globalProperties.isLoggedIn = !!localStorage.jwt;
