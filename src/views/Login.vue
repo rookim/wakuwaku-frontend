a7b8429106468b6cc964f4024eb7fcf799310819
ro <kronew6@gmail.com>
2022-03-23 12:30:50 -0500

    adds body tag

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -39,71 +39,73 @@ export default {
 </script>
 
 <template>
-  <div class="main-container p-0">
-    <section id="contact" class="lg">
-      <div class="container text-center">
-        <h1>LOGIN</h1>
-        <div class="row v-center">
-          <!-- / column -->
-          <div class="col-lg-5 mx-auto">
-            <div class="promo-box">
-              <form
-                class="needs-validation"
-                id="form-validation5"
-                novalidate="novalidate"
-                v-on:submit.prevent="submit()"
-              >
-                <div class="row">
-                  <ul>
-                    <li v-for="error in errors" v-bind:key="error.id">{{ error }}</li>
-                  </ul>
-                  <div class="col-md-12">
-                    <div class="form-group">
-                      <input
-                        v-model="newSessionParams.email"
-                        type="text"
-                        class="form-control"
-                        id="contact-subject5"
-                        name="inputSubject5"
-                        placeholder="&#xf2bd; Email"
-                        style="font-family: 'Font Awesome 5 Free', sans-serif !important; font-weight: 400"
-                      />
+  <body>
+    <div class="main-container p-0">
+      <section id="contact" class="lg">
+        <div class="container text-center">
+          <h1>LOGIN</h1>
+          <div class="row v-center">
+            <!-- / column -->
+            <div class="col-lg-5 mx-auto">
+              <div class="promo-box">
+                <form
+                  class="needs-validation"
+                  id="form-validation5"
+                  novalidate="novalidate"
+                  v-on:submit.prevent="submit()"
+                >
+                  <div class="row">
+                    <ul>
+                      <li v-for="error in errors" v-bind:key="error.id">{{ error }}</li>
+                    </ul>
+                    <div class="col-md-12">
+                      <div class="form-group">
+                        <input
+                          v-model="newSessionParams.email"
+                          type="text"
+                          class="form-control"
+                          id="contact-subject5"
+                          name="inputSubject5"
+                          placeholder="&#xf2bd; Email"
+                          style="font-family: 'Font Awesome 5 Free', sans-serif !important; font-weight: 400"
+                        />
+                      </div>
+                      <!-- / form-group -->
                     </div>
-                    <!-- / form-group -->
-                  </div>
-                  <!-- / column -->
-                  <div class="col-md-12">
-                    <div class="form-group">
-                      <input
-                        v-model="newSessionParams.password"
-                        type="password"
-                        class="form-control"
-                        id="contact-subject5"
-                        name="inputSubject5"
-                        placeholder="&#xf059; Password"
-                        style="font-family: 'Font Awesome 5 Free', sans-serif !important; font-weight: 400"
-                      />
+                    <!-- / column -->
+                    <div class="col-md-12">
+                      <div class="form-group">
+                        <input
+                          v-model="newSessionParams.password"
+                          type="password"
+                          class="form-control"
+                          id="contact-subject5"
+                          name="inputSubject5"
+                          placeholder="&#xf059; Password"
+                          style="font-family: 'Font Awesome 5 Free', sans-serif !important; font-weight: 400"
+                        />
+                      </div>
+                      <!-- / form-group -->
                     </div>
-                    <!-- / form-group -->
+                    <!-- / column -->
                   </div>
-                  <!-- / column -->
-                </div>
-                <!-- / row -->
+                  <!-- / row -->
 
-                <input v-on:click="submit()" type="submit" class="btn btn-primary btn-submit" value="Submit" />
-              </form>
-              <!-- / form-group -->
+                  <input v-on:click="submit()" type="submit" class="btn btn-primary btn-submit" value="Submit" />
+                </form>
+                <!-- / form-group -->
+              </div>
+              <!-- / promo-box -->
             </div>
-            <!-- / promo-box -->
+            <!-- / column -->
           </div>
-          <!-- / column -->
+          <!-- / row -->
         </div>
-        <!-- / row -->
-      </div>
-      <!-- / container -->
-    </section>
-    <!-- / contact -->
-  </div>
+        <!-- / container -->
+      </section>
+      <!-- / contact -->
+    </div>
+  </body>
 
   <!-- OLD CODE -->
   <!-- <div class="login">
