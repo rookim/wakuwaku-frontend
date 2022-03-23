d2e517b06f8ef10e2dc546560889ac8b21e012ea
ro <kronew6@gmail.com>
2022-03-23 11:51:00 -0500

    adds styling using MIPO theme

--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -22,13 +22,65 @@ export default {
 </script>
 
 <template>
-  <!-- class names in kebab case or no? YES-->
+  <body>
+    <header class="lg bg-black v-center">
+      <div class="container">
+        <div class="header-content text-center">
+          <h2 class="page-title mb-40 text-white">MY ACCOUNT</h2>
+        </div>
+        <!-- / header-content -->
+      </div>
+      <!-- / container -->
+    </header>
+    <div class="main-container p-0">
+      <section id="project-info" class="lg">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-5 mx-auto">
+              <div class="promo-box project-info sticky-top top-30">
+                <h2 class="mb-15">Account Details</h2>
+
+                <div class="spacer spacer-line-fw border-faded">&nbsp;</div>
+
+                <ul class="list-unstyled">
+                  <li class="fs-16 mb-15 title-color">
+                    <span class="fw-medium" v-if="currentUser.email">Email:</span>
+                    {{ currentUser.email }}
+                  </li>
+                  <li class="fs-16 mb-15 title-color">
+                    <span class="fw-medium" v-if="currentUser.username">Username:</span>
+                    {{ currentUser.username }}
+                  </li>
+                  <li class="fs-16 mb-15 title-color">
+                    <span class="fw-medium" v-if="currentUser.phone_number">Phone number:</span>
+                    {{ currentUser.phone_number }}
+                  </li>
+                </ul>
+
+                <div class="spacer spacer-line-fw border-faded">&nbsp;</div>
+
+                <router-link to="/me/edit" class="btn btn-lg btn-primary ml-0 m-5">Edit</router-link>
+              </div>
+              <!-- / promo-box -->
+            </div>
+            <!-- / column -->
+          </div>
+          <!-- / row -->
+        </div>
+        <!-- / container -->
+      </section>
+      <!-- / project-info -->
+    </div>
+    <!-- main-container -->
+  </body>
+
+  <!-- OLD CODE
+  class names in kebab case or no? YES
   <div class="users-show">
     <h1>Account</h1>
-    <!-- v-if logic to not show all this information if user not logged in (for now) -->
     <p v-if="currentUser.email">Email: {{ currentUser.email }}</p>
     <p v-if="currentUser.username">Username: {{ currentUser.username }}</p>
     <p v-if="currentUser.phone_number">Phone number: {{ currentUser.phone_number }}</p>
     <router-link to="/me/edit">Edit</router-link>
-  </div>
+  </div> -->
 </template>
